2543add6b88d59a3f7a389ba6863f6e826ca91d7
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-03-11 15:06:35 +0000

    Re #25074 fixed windows issue

--- qt/scientific_interfaces/Indirect/IndirectSpectrumSelectionPresenter.cpp
+++ qt/scientific_interfaces/Indirect/IndirectSpectrumSelectionPresenter.cpp
@@ -255,8 +255,9 @@ IndirectSpectrumSelectionPresenter::validate(UserInputValidator &validator) {
 
   const auto numberOfHistograms =
       m_model->getWorkspace(m_activeIndex)->getNumberHistograms();
-  const auto expected = m_view->maximumSpectrum() - m_view->minimumSpectrum()+1;
-  if (hist < expected) {
+  const auto expected =
+      m_view->maximumSpectrum() - m_view->minimumSpectrum() + 1;
+  if (numberOfHistograms < expected) {
     validator.addErrorMessage(
         QString::fromStdString("Spectra range entered is greater than spectra "
                                "range of sample workspace"));
